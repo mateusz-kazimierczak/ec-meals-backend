8d433c355736dd7bd84cd5bfb0ff292ed88f98d6
Mateusz Kazimierczak <mateuszkazimierczak@Mateuszs-MacBook-Pro.local>
2024-05-15 21:18:42 -0400

    fixed timezone errors

--- src/app/api/meals/route.js
+++ src/app/api/meals/route.js
@@ -1,17 +1,32 @@
 import connectDB from "@/_helpers/db/connect";
 import User from "@/_helpers/db/models/User";
+import { get } from "http";
 
 const getNextUpdateTime = () => {
   const currTime = new Date();
   const nextUpdateTime = new Date();
 
-  nextUpdateTime.setHours(
-    parseInt(process.env.UPDATE_TIME.slice(0, 2)),
-    parseInt(process.env.UPDATE_TIME.slice(2)),
-    0,
-    0
-  );
-  nextUpdateTime.setDate(nextUpdateTime.getDate() + 1);
+  const timezoneOffset = new Date().getTimezoneOffset();
+
+  const nextUpdateHour =
+    parseInt(process.env.UPDATE_TIME.slice(0, 2)) + timezoneOffset / 60 - 4; // 4 is the offset for EST
+
+  if (nextUpdateHour < 0) {
+    nextUpdateTime.setHours(
+      24 + nextUpdateHour,
+      parseInt(process.env.UPDATE_TIME.slice(2)),
+      0,
+      0
+    );
+  } else {
+    nextUpdateTime.setHours(
+      parseInt(nextUpdateHour), // 4 is the offset for EST
+      parseInt(process.env.UPDATE_TIME.slice(2)),
+      0,
+      0
+    );
+    nextUpdateTime.setDate(nextUpdateTime.getDate() + 1);
+  }
 
   if (currTime - nextUpdateTime > 0) {
     nextUpdateTime.setDate(nextUpdateTime.getDate() + 1);
