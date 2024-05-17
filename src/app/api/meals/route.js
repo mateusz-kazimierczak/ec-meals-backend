ce4d56f4d332e2c4764618f486cd248d526cfcfb
Mateusz Kazimierczak <mateuszkazimierczak@Mateuszs-MacBook-Pro.local>
2024-05-17 06:36:47 -0400

    another build attempt

--- src/app/api/meals/route.js
+++ src/app/api/meals/route.js
@@ -2,47 +2,22 @@ import connectDB from "@/_helpers/db/connect";
 import User from "@/_helpers/db/models/User";
 
 const getNextUpdateTime = () => {
-  // Get the time until the next update, and the index of the day that should be disabled
-  const currentStandardTime = new Date();
-  const timezoneOffsetHours = currentStandardTime.getTimezoneOffset() / 60;
-  const desiredOffset = -4; // The desired timezone offset
-
-  // Calculate the difference between the current timezone offset and the desired timezone offset
-  const offsetDifference = timezoneOffsetHours - desiredOffset;
-
-  // Create a new Date object with the adjusted time
-  const currTime = new Date(
-    currentStandardTime.getTime() - offsetDifference * 60 * 60 * 1000
-  );
-
+  const currTime = new Date();
   const nextUpdateTime = new Date();
-  let disabledDay;
-
-  const nextUpdateHour =
-    parseInt(process.env.UPDATE_TIME.slice(0, 2)) -
-    currTime.getTimezoneOffset() / 60 +
-    4;
 
   nextUpdateTime.setHours(
-    nextUpdateHour,
+    parseInt(process.env.UPDATE_TIME.slice(0, 2)),
     parseInt(process.env.UPDATE_TIME.slice(2)),
     0,
     0
   );
+  nextUpdateTime.setDate(nextUpdateTime.getDate() + 1);
 
   if (currTime - nextUpdateTime > 0) {
     nextUpdateTime.setDate(nextUpdateTime.getDate() + 1);
-    disabledDay = new Date().getDay() - 1;
-  } else {
-    disabledDay = new Date().getDay() - 2;
   }
 
-  if (disabledDay == -1) disabledDay = 6;
-  else if (disabledDay == -2) disabledDay = 5;
-
-  console.log("Next update time: ", nextUpdateTime);
-
-  return [nextUpdateTime.getTime(), disabledDay];
+  return nextUpdateTime.getTime();
 };
 
 export async function GET(req, res) {
@@ -57,21 +32,33 @@ export async function GET(req, res) {
     return Response.json({ message: "Unauthorized" }, { status: 403 });
   }
 
-  console.log("get meals route");
+  console.log(forUser);
 
   const data = await User.findById(forUser, "meals firstName");
 
-  if (!data) {
-    return Response.json({ message: "User not found" }, { status: 404 });
+  let disabledDay;
+
+  const updateTimeToday = new Date();
+  updateTimeToday.setHours(
+    parseInt(process.env.UPDATE_TIME.slice(0, 2)),
+    parseInt(process.env.UPDATE_TIME.slice(2)),
+    0,
+    0
+  );
+
+  if (Date.now > updateTimeToday) {
+    disabledDay = new Date().getDay();
+  } else {
+    disabledDay = new Date().getDay() - 1;
   }
 
-  const [updateTime, disabledDay] = getNextUpdateTime();
+  if (disabledDay < 0) disabledDay = 6;
 
   return Response.json({
     meals: data.meals,
     firstName: data.firstName,
     currTime: new Date(),
-    updateTime: updateTime,
+    updateTime: getNextUpdateTime(),
     disabledDay,
   });
 }
