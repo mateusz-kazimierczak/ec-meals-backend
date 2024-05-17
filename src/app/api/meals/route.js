be75c13bffc8c571a3c79ddf2009584d42d1891f
Mateusz Kazimierczak <mateuszkazimierczak@Mateuszs-MacBook-Pro.local>
2024-05-17 07:05:09 -0400

    attempt vercel fix

--- src/app/api/meals/route.js
+++ src/app/api/meals/route.js
@@ -2,22 +2,47 @@ import connectDB from "@/_helpers/db/connect";
 import User from "@/_helpers/db/models/User";
 
 const getNextUpdateTime = () => {
-  const currTime = new Date();
+  // Get the time until the next update, and the index of the day that should be disabled
+  const currentStandardTime = new Date();
+  const timezoneOffsetHours = currentStandardTime.getTimezoneOffset() / 60;
+  const desiredOffset = -4; // The desired timezone offset
+
+  // Calculate the difference between the current timezone offset and the desired timezone offset
+  const offsetDifference = timezoneOffsetHours - desiredOffset;
+
+  // Create a new Date object with the adjusted time
+  const currTime = new Date(
+    currentStandardTime.getTime() - offsetDifference * 60 * 60 * 1000
+  );
+
   const nextUpdateTime = new Date();
+  let disabledDay;
+
+  const nextUpdateHour =
+    parseInt(process.env.UPDATE_TIME.slice(0, 2)) -
+    currTime.getTimezoneOffset() / 60 +
+    4;
 
   nextUpdateTime.setHours(
-    parseInt(process.env.UPDATE_TIME.slice(0, 2)),
+    nextUpdateHour,
     parseInt(process.env.UPDATE_TIME.slice(2)),
     0,
     0
   );
-  nextUpdateTime.setDate(nextUpdateTime.getDate() + 1);
 
   if (currTime - nextUpdateTime > 0) {
     nextUpdateTime.setDate(nextUpdateTime.getDate() + 1);
+    disabledDay = new Date().getDay() - 1;
+  } else {
+    disabledDay = new Date().getDay() - 2;
   }
 
-  return nextUpdateTime.getTime();
+  if (disabledDay == -1) disabledDay = 6;
+  else if (disabledDay == -2) disabledDay = 5;
+
+  console.log("Next update time: ", nextUpdateTime);
+
+  return [nextUpdateTime.getTime(), disabledDay, currTime];
 };
 
 export async function GET(req, res) {
@@ -32,33 +57,21 @@ export async function GET(req, res) {
     return Response.json({ message: "Unauthorized" }, { status: 403 });
   }
 
-  console.log(forUser);
+  console.log("get meals route");
 
   const data = await User.findById(forUser, "meals firstName");
 
-  let disabledDay;
-
-  const updateTimeToday = new Date();
-  updateTimeToday.setHours(
-    parseInt(process.env.UPDATE_TIME.slice(0, 2)),
-    parseInt(process.env.UPDATE_TIME.slice(2)),
-    0,
-    0
-  );
-
-  if (Date.now > updateTimeToday) {
-    disabledDay = new Date().getDay();
-  } else {
-    disabledDay = new Date().getDay() - 1;
+  if (!data) {
+    return Response.json({ message: "User not found" }, { status: 404 });
   }
 
-  if (disabledDay < 0) disabledDay = 6;
+  const [updateTime, disabledDay, currTime] = getNextUpdateTime();
 
   return Response.json({
     meals: data.meals,
     firstName: data.firstName,
-    currTime: new Date(),
-    updateTime: getNextUpdateTime(),
+    currTime: currTime,
+    updateTime: updateTime,
     disabledDay,
   });
 }
@@ -76,6 +89,8 @@ export async function POST(req, res) {
   }
 
   const data = await req.json();
+
+  console.log("post meals route", data);
 
   const user = await User.findByIdAndUpdate(forUser, { meals: data.meals });
 
